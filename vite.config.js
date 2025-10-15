03f2c42e11bf9d2be07d7782030451203ccc37c0
hvl2042 <hvl2042@gepplus.net>
2025-10-15 10:09:37 +0900

    🎉 Begin a project

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "icons/favicon-32x32.png",
+        "icons/apple-touch-icon-180x180.png",
+      ],
+      manifest: {
+        name: "Weatherly",
+        short_name: "Weatherly",
+        description: "A weather forecast app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/apple-touch-icon-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
