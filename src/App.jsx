195e93f3ec230772665fd36a92b6854123ed866a
hvl2042 <hvl2042@gepplus.net>
2025-10-22 10:07:41 +0900

    ✨ Update App.jsx : use createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import TodayPage from "./pages/TodayPage";
 import MonthlyPage from "./pages/MonthlyPage";
 import TodoPage from "./pages/TodoPage";
 import BottomNav from "./components/BottomNav";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/today" replace />,
+  },
+  {
+    path: "/today",
+    element: (
+      <>
+        <TodayPage />
+        <BottomNav />
+      </>
+    ),
+  },
+  {
+    path: "/monthly",
+    element: (
+      <>
+        <MonthlyPage />
+        <BottomNav />
+      </>
+    ),
+  },
+  {
+    path: "/todo",
+    element: (
+      <>
+        <TodoPage />
+        <BottomNav />
+      </>
+    ),
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter>
-      <div style={{ paddingBottom: "60px" }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/today" />} />
-          <Route path="/today" element={<TodayPage />} />
-          <Route path="/monthly" element={<MonthlyPage />} />
-          <Route path="/todo" element={<TodoPage />} />
-        </Routes>
-        <BottomNav />
-      </div>
-    </BrowserRouter>
+    <div style={{ paddingBottom: "60px" }}>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
